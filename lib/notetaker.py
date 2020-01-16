bfcf21964ebcb712bc143fcb2c22d820f5d413f3
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-16 13:00:15 -0500

    created peewee class but it cannot import peewee. I will try to install dependences not to lib but to the project folder

--- lib/notetaker.py
+++ lib/notetaker.py
@@ -1,3 +1,29 @@
 import os
-import sqlite3
 from termcolor import colored
+from peewee import *
+from datetime import date
+
+db = PostgresqlDatabase('people', user='postgres', password='',
+                        host='localhost', port=3000)
+
+db.connect()
+
+
+class BaseModel(Model):
+    """A base model that will use our Postgresql database. We don't have to do
+    this, but it makes connecting models to our database a lot easier."""
+    class Meta:
+        database = db
+
+
+class Notes(BaseModel):
+    id = CharField(primary_key=True)
+    description = CharField(unique=True)
+    category = CharField(choices=True)
+    deadline = DateField()
+    importance = IntegerField(validate_range(low=1, high=5))
+    still_actual = BooleanField()
+
+
+db.drop_tables([Notes])
+db.create_tables([Notes])
