5da838d88d59366c8b84df528a559526e780c4b0
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-19 19:40:50 -0500

    put update in separate function. It does not work properly

--- lib/notetaker.py
+++ lib/notetaker.py
@@ -61,6 +61,31 @@ def create():
     return True
 
 
+def update():
+    update = input(
+        "Please chose what you would like to update: \n category (print 1) \n note (print 2) \n if the note is still actual (print 3) \n")
+    if update == "1":
+        chose_category = input(
+            "What category you would like to update: ")
+        new_category = input("Please enter a new category: ")
+        update_category = Notes.get(Notes.category == chose_category)
+        update_category.save()
+        print(
+            f"Successfully updated from from {chose_category} to {new_category}")
+        return True
+    elif update == "2":
+        chose_description = input(
+            "What note you would like to update: ")
+        new_description = input("Please enter a new note: ")
+        update_description = Notes.get(
+            Notes.description == chose_description)
+        update_description.description = new_description
+        update_description.save()
+        print(
+            "Successfully updated")
+        return True
+
+
 def chose_action():
     action = input(
         "What do you want to do?: \n see all the notes (type: 1), \n delete notes (type: 2), \n create a note (type: 3) \n update existing note (type: 4) \n exit(type: 5) \n ")
@@ -71,83 +96,14 @@ def chose_action():
     elif action == '3':
         return create()
     elif action == '4':
-        update = input(
-            "Please chose what you would like to update: \n category (print 1) \n note (print 2) \n if the note is still actual (print 3) \n")
-        if update == "1":
-            chose_category = input(
-                "What category you would like to update: ")
-            new_category = input("Please enter a new category: ")
-            update_category = Notes.get(Notes.category == chose_category)
-            update_category.save()
-            print(
-                f"Successfully updated from from {chose_category} to {new_category}")
-        elif update == "2":
-            chose_description = input(
-                "What note you would like to update: ")
-            new_description = input("Please enter a new note: ")
-            update_description = Notes.get(
-                Notes.description == chose_description)
-            update_description.description = new_description
-            update_description.save()
-        print(
-            f"Successfully updated")
-        return True
+        return update()
     elif action == '5':
         print("See you next time!")
-        #raise SystemExit
+        # raise SystemExit
         return False
     else:
         print("You have made a mistake. Please type carefully")
         return True
-
-
-# def see_all():
-
-    # one_more = input("Do you want to see smth else (y or n? ")
-    # if one_more == "y":
-    #     see_all()
-    # elif one_more == "n":
-    #     chose_action()
-    # else:
-    #     print("You have a mistake. Please type carefully")
-
-
-# chose_action()
-
-
-# search_word = input("Please enter a word for search \n ")
-# search_result = Notes.select().where(
-#     Notes.description.contains(f"{search_word}"))
-# print(search_result)
-
-
-#     search_word = input("Please enter a word for search")
-
-
-# for notes in Notes.select().where(Notes.description == test)
-# print(Notes.description)
-
-# search_result = Notes.where(Notes.description.name.contains("{search_word}"))
-# print(search_result)
-
-
-# def delete():
-#     print("delete")
-
-
-# def create():
-#     new_description = input("Please enter your note: ")
-#     new_category = input("Please enter category: ")
-#     new_still_actual = "Yes"
-
-#     new_note = Notes(description=new_description,
-#                      category=new_category, still_actual=new_still_actual)
-#     new_note.save()
-#     print(
-#         f"Here is your note with category {new_note.category}: {new_note.description}")
-
-
-# chose_action()
 
 
 while True:
