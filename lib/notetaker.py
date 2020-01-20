467f44812eedc96ee8207c95bfd894afd0712479
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-19 19:15:07 -0500

    added returns

--- lib/notetaker.py
+++ lib/notetaker.py
@@ -38,12 +38,14 @@ def chose_action():
         for note in search_results:
             print("Your note: "+note.description + "." +
                   " Is it still actual? " + note.still_actual+".")
+        return True
     elif action == '2':
         delete_category = input(
             "Please enter the category you want to delete: ")
         delete_category = Notes.get(Notes.category == delete_category)
         delete_category.delete_instance()
         print(f"Your note titled {delete_category} has been deleted")
+        return True
     elif action == '3':
         new_description = input("Please enter your note: ")
         new_category = input("Please enter category: ")
@@ -53,6 +55,7 @@ def chose_action():
         new_note.save()
         print(
             f"Here is your note with category {new_note.category}: {new_note.description}")
+        return True
     elif action == '4':
         update = input(
             "Please chose what you would like to update: \n category (print 1) \n note (print 2) \n if the note is still actual (print 3) \n")
@@ -74,11 +77,14 @@ def chose_action():
             update_description.save()
         print(
             f"Successfully updated")
+        return True
     elif action == '5':
         print("See you next time!")
-        raise SystemExit
+        #raise SystemExit
+        return False
     else:
         print("You have made a mistake. Please type carefully")
+        return True
 
 
 # def see_all():
@@ -130,52 +136,8 @@ def chose_action():
 # chose_action()
 
 
-# def update():
-#     update = input(
-#         "Please chose what you would like to update: \n category (print 1) \n note (print 2) \n if the note is still actual (print 3) \n")
-#     if update == "1":
-#         chose_category = input(
-#             "What category you would like to update: ")
-#         new_category = input("Please enter a new category: ")
-#         update_category = Notes.get(Notes.category == chose_category)
-#         # for category in update_category:
-#         #     update_category.category = new_category
-#         update_category.save()
-#         print(
-#             f"Successfully updated from from {chose_category} to {new_category}")
-#     elif update == "2":
-#         chose_description = input(
-#             "What note you would like to update: ")
-#         new_description = input("Please enter a new note: ")
-#         update_description = Notes.get(Notes.description == chose_description)
-#         update_description.description = new_description
-#         update_description.save()
-#         print(
-#             f"Successfully updated from from {chose_description} to {new_description}")
-
 while True:
-    chose_action()
+    cont = chose_action()
     # print("loop")
-
-# print("loop ended\n")
-
-# chose_action()
-
-
-# def go_out():
-#     print("See you next time!")
-
-
-# chose_action()
-# see_all()
-# delete()
-# create()
-# update()
-# go_out()
-
-
-# for filtering using still_active true
-# User.select().where(User.active == True)
-
-# random line
-# LotteryNumber.select().order_by(fn.Random()).limit(5)
+    if not cont:
+        break
